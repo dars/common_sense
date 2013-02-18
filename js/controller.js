dc6a190f29b1822bb32eff7318561f9274308fae
Dars <dars94@gmail.com>
2013-02-19 02:55:23 +0800

    controller function 改以 closure 寫法, angular.module

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,6 @@
-function commonForm($scope)
-{
+var ctrl = angular.module('commonApp', []);
+
+ctrl.controller('commonForm', ['$scope', function($scope){
     $scope.things = "";
     $scope.commonThings = [];
     $scope.getLists = function()
@@ -88,7 +89,99 @@ function commonForm($scope)
     {
         item.isDelete = true;
     }
-}
+}]);
+
+// function commonForm($scope)
+// {
+//     $scope.things = "";
+//     $scope.commonThings = [];
+//     $scope.getLists = function()
+//     {
+//         var msgs = new Message();
+//         var q = new StackMob.Collection.Query();
+//         q.orderDesc('createddate');
+//         $(".alert").show();
+//         msgs.query(q, {
+//             success: function(model) {
+//                 var obj = model.toJSON();
+//                 var res = [];
+//                 $.each(obj, function(i, o){
+//                     o.isDelete = false;
+//                     o.timestamp = getDatetime(o.createddate);
+//                     res.push(o);
+//                 });
+//                 $scope.$apply(function() {
+//                     $scope.commonThings = res;
+//                 });
+//                 $(".alert").hide();
+//             },
+//             error: function(model, response) {
+//                 $(".alert").hide();
+//             }
+//         });
+//     }
+//     $scope.getLists();
+//     $scope.addThing = function()
+//     {
+//         if(this.things){
+//             $(".alert").show();
+//             var to = new Message({
+//                 content: this.things,
+//                 agree:0,
+//                 disagree:0
+//             });
+//             to.create({
+//                 success:function(model) {
+//                     $scope.getLists();
+//                     console.debug(model.toJSON());
+//                     $(".alert").hide();
+//                 },
+//                 error : function(model, response) {
+//                     console.debug("Oops there was an error in creating the object.");
+//                     console.debug("Have you initialized your JS SDK?");
+//                     console.debug("Are you running this on StackMob's Local Runner?");
+//                     console.debug("Are you running this on StackMob's GitHub-integrated hosting?")
+//                     console.debug(response);
+//                     $(".alert").hide();
+//                 }
+//             });
+//             this.things = "";
+//         }
+//     }
+
+//     $scope.agree_btn = function(item)
+//     {
+//         var query = new Message({message_id:item.message_id, agree:item.agree+1});
+//         query.save({}, {
+//             success: function(model) {
+//                 $scope.getLists();
+//                 console.debug(model.toJSON());
+//             },
+//             error: function(model, response) {
+//                 console.debug(response);
+//             }
+//         });
+//     }
+
+//     $scope.disagree_btn = function(item)
+//     {
+//         var query = new Message({message_id:item.message_id, disagree:item.disagree+1});
+//         query.save({}, {
+//             success: function(model) {
+//                 $scope.getLists();
+//                 console.debug(model.toJSON());
+//             },
+//             error: function(model, response) {
+//                 console.debug(response);
+//             }
+//         });
+//     }
+
+//     $scope.removeThing = function(item)
+//     {
+//         item.isDelete = true;
+//     }
+// }
 
 // 改用 moment.js
 function getDatetime(utc_sec)
